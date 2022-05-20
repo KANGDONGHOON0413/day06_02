04af13757285c49f58cba041ca39486a38c60b89
KANGDONGHOON0413 <kmerits@gmail.com>
2022-05-20 23:56:53 +0900

    초기 커밋.

---
+++ day06_02/Computer01.cs
@@ -0,0 +1,40 @@
+﻿#define ALPHA   //이 파일에서만 사용가능
+#undef DEBUG    //DEBUG는 사용하지 않겠다는 의미
+
+#pragma warning disable 23
+//23번라인의 경고를 무시한다
+
+using System;
+using static day06_02.Computer02;
+
+//전처리기 연습
+namespace day06_02
+{
+    class Computer01
+    {
+
+        static void Main(string[] args)
+        {
+            bool verbose = false;
+#if(ALPHA)
+            Console.WriteLine("test environment: verbose opthion is set");
+            verbose = true;
+#else
+            Console.WriteLine("production");
+#endif
+
+            //region으로 소스코드를 효율적으로 관리할 수 있다. 아래와 같이.
+            #region Starting_Verbose
+            if (verbose)
+            {
+
+                Console.WriteLine("verbose mode");
+#if(!DEBUG)
+                var Server = Computer02.Server();
+                Console.WriteLine(Server);
+#endif
+                #endregion
+            }
+        }
+    }
+}
---
+++ day06_02/Computer02.cs
@@ -0,0 +1,41 @@
+﻿#define BETA
+//#define PROTECTION_KEY01
+#define PROTECTION_KEY02
+
+
+//빌드는 허용하지만 경고를 주는 warning메서드와
+#region warning_method
+#if(!PROTECTION_KEY01)
+#warning this class should used in PROTECTION_KEY01
+#endif
+#endregion
+
+
+//빌드도 허용하지 않는 error메서드가 있다.
+#region error_method
+#if(!PROTECTION_KEY02)
+#error this class Must used in PROTECTION_KEY02
+#endif 
+#endregion
+using System;
+
+namespace day06_02
+{
+    public class Computer02
+    {
+        public static string Server()
+        {
+            string server;
+#if(ALPHA)
+            server = "Alpha testing....";
+
+#elif (BETA)
+            server = "Beta testing....";
+#else
+            server = "Operating....";
+#endif
+            return server;
+        }
+    }
+}
+
